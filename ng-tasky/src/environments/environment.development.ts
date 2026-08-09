9a60d08507026d6a8d9d155e1399d955f1b1711a
Babie-cloud <karliegionacubahiro@gmail.com>
2026-08-09 17:12:50 +0200

    new commit

--- ng-tasky/src/environments/environment.development.ts
+++ ng-tasky/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: '',
   googleClientId: '807753832705-n8pgdoae7bdfjku47jv93v151dla23pg.apps.googleusercontent.com',
   facebookAppId: '2094027974853072',
 };
