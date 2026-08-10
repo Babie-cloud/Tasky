d1fb8011046154c2caa5320738330cafb23a935b
Babie-cloud <karliegionacubahiro@gmail.com>
2026-08-10 23:57:01 +0200

    updating routes

--- ng-tasky/src/app/app.routes.ts
+++ ng-tasky/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-//import { Landingpage } from './auth/landingpage/landingpage';
 import { Login } from './auth/login/login';
 import { Signup } from './auth/signup/signup';
 import { Resetpassword } from './auth/resetpassword/resetpassword';
@@ -11,8 +10,9 @@ import { Pricing } from './components/pricing/pricing';
 import { AcceptInvite } from './components/accept-invite/accept-invite';
 import { authGuard } from './core/guard/auth-guard';
 import { Profile } from './components/profile/profile';
+import { PolicyPrivacy } from './layout/policy-privacy/policy-privacy';
+import { Terms } from './layout/terms/terms';
 export const routes: Routes = [
-    //{ path: '', component: Landingpage },
     { path: '', component: Homepage },
     { path: 'login', component: Login},
     { path: 'signup', component: Signup},
@@ -26,6 +26,8 @@ export const routes: Routes = [
     { path: 'add-task', component: AddTask },
     { path: 'boards/accept-invite', component: AcceptInvite, canActivate: [authGuard] },
     { path: 'profile', component: Profile},
+     { path: 'terms', component: Terms },
+      { path: 'policy-privacy', component: PolicyPrivacy },
     ]
     },
     
