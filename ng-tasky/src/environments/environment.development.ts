3a5be5233a53d68b66f4a641e425cbd03833d38c
Babie-cloud <karliegionacubahiro@gmail.com>
2026-08-12 21:19:20 +0200

    auth access

--- ng-tasky/src/environments/environment.development.ts
+++ ng-tasky/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  apiUrl: '',
+  production: false,
+  apiUrl: 'http://localhost:3000',
   googleClientId: '807753832705-n8pgdoae7bdfjku47jv93v151dla23pg.apps.googleusercontent.com',
   facebookAppId: '2094027974853072',
 };
