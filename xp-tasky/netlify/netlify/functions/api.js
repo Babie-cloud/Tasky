9f88e27f7c57c1c93c46784d4dead476bfb84ad8
Babie-cloud <karliegionacubahiro@gmail.com>
2026-08-12 21:37:09 +0200

    auth access

--- xp-tasky/netlify/netlify/functions/api.js
+++ xp-tasky/netlify/netlify/functions/api.js
@@ -2,5 +2,5 @@ const serverless = require('serverless-http');
 const app = require('../../app');
 
 module.exports.handler = serverless(app, {
-  basePath: '/.netlify/functions/api',
+  basePath: '/.netlify/functions',
 });
