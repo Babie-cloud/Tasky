c1629d60180ceabba28e299c5ec1849d63eb0677
Babie-cloud <karliegionacubahiro@gmail.com>
2026-09-07 16:43:13 +0200

    correcting things

--- xp-tasky/app.js
+++ xp-tasky/app.js
@@ -18,7 +18,6 @@ var listsRouter = require('./routes/lists');
 var billingRouter = require('./routes/billing');
 var stripeWebhookRouter = require('./routes/stripe-webhook');
 var categoriesRouter = require('./routes/categories');
-
 const app = express();
 
 const allowedOrigins = [
